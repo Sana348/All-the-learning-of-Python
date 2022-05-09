b7c385a3a0f48eab5199bee4fc8fce6033912e0a
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2022-05-09 11:09:51 +0530

    Add files via upload
    initial commit

---
+++ python-3-boot-camp-in-gui-automation.py.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import smtplib
+conn=smtplib.SMTP("smtp.gmail.com",465)
+conn.ehlo()
+conn.starttls()
+conn.login("misana348@gmail.com","sana.348")
+conn.sendmail("misana348@gmail.com","asifabricator@gmail.com","Subject: LEARNING /n/n Body: enjoy the process /n/n -sana")
+
+
+# In[2]:
+
+
+import smtplib
+conn=smtplib.SMTP("smtp.gmail.com",465)
+conn.ehlo()
+conn.starttls()
+conn.login("misana348@gmail.com","sana.348")
+conn.sendmail("misana348@gmail.com","asifabricator@gmail.com","Subject: LEARNING /n/n Body: enjoy the process /n/n -sana")
+
+
+# In[3]:
+
+
+import pyautogui
+
+print (pyautogui.position())
+pyautogui.move To (61,25 duration=5)
+pyautogui.click()
+pyautogui.move To (140,85, duration=1)
+pyautogui.click()
+pyautogui.move To (100,70, duration=1)
+pyautogui.click()
+
+
+# In[1]:
+
+
+import pyautogui
+pyautogui.click()
+distance 200
+while distance>8:
+print(distance,e)
+pyautogui.dragRel(-distance, duration=1)
+distance distance-25
+print(0 distance)
+pyautogui.dragRel(e distance, duration=1)
+print(-distance 0)
+pyautogui.dragRel(distance e duration=1)
+distance distance-25
+ print(0, -distance)
+pyautogui.dragRel(e-distance duration=1)
+
+#pyautogui.hotkey('shift', 'fn', 'f9')
+#pyautogui.hotkey ('ctrl', 'fn', 'f2')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

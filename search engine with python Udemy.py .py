39c4191619b2885d8b72a0749429581e6d025e1f
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2022-05-09 11:05:34 +0530

    initial commit

---
+++ search engine with python Udemy.py .py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(7*7*24*60)
+
+
+# In[2]:
+
+
+print(299792458*100*1/1000000000)
+
+
+# In[3]:
+
+
+speed_of_light = 299792458
+x = speed_of_light*100
+
+print(x)
+billionth = 1/1000000000
+distance = 29.9
+
+
+# In[5]:
+
+
+speed_of_light = 299792458 #meter per sec.
+cycle_of_second = 2700000000 #2.7 ghz
+cycle_distance =speed_of_light/cycle_of_second
+print(cycle_distance*100)
+
+
+# In[6]:
+
+
+x=11
+x=x+1
+x=x*2
+
+
+# In[7]:
+
+
+x
+
+
+# In[10]:
+
+
+
+
+hours = hours +1
+minutes = hours*60
+
+
+# In[11]:
+
+
+age = 21
+days_in_year = 365
+print(age*days_in_year)
+
+
+# In[1]:
+
+
+s = 'My name is SANA MIRZA'
+s.find('N')
+
+
+# In[6]:
+
+
+def inc (n):
+    return n + 1
+
+
+# In[10]:
+
+
+def sum(a,b):
+    a = a+b
+    
+
+
+# In[11]:
+
+
+def sum(a,b):
+    a = a+b
+    
+
+
+# In[12]:
+
+
+def sum(c,d):
+    a = c+d
+
+
+# In[17]:
+
+
+def sum(a,b):
+    a = a+b
+    return a
+a='sana'
+b='mirza'
+print(sum(a,b))
+print(b)
+
+
+# In[4]:
+
+
+def sum3(a,b,c):
+    return(a+b+c)
+a = '3'
+b = '2'
+c = '2'
+print (sum(a,b,c))
+
+
+# In[9]:
+
+
+def double_string(a,b):
+    return a+b+b+a
+print (double_string('a','b')) 
+print (double_string('i','love'))
+
+
+# In[34]:
+
+
+def is_friend(Name):
+    if Name[0]=='M':
+        
+        return True
+    else:
+        return False
+
+
+# In[35]:
+
+
+def is_friend(Name):
+    if Name[0]=='M':
+        
+        return True
+    else:
+        return False
+
+
+# In[38]:
+
+
+i = 10
+while i > 10:
+    print = i
+    i=i+1
+    
+
+
+# In[37]:
+
+
+i
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
